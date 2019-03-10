43663c5256b556697cb34ebfc8699b69ccbe09fd
VinsW <vins.im.ruh@gmail.com>
2019-03-10 19:15:57 +0530

    Update PropertyReader.java

--- src/main/java/core/common/PropertyReader.java
+++ src/main/java/core/common/PropertyReader.java
@@ -1,15 +1,19 @@
 package core.common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PropertyReader {
 
     private Map<String, WebElement> fieldMap = null;
 
     /**
-     * This method is used to retrieve a element by using displayed field name from a
+     * This method is used to retrieve a element by using displayed field name from a table form
      */
 
     public WebElement getCellEditor(String fieldName) {
@@ -19,8 +23,28 @@ public class PropertyReader {
     private WebElement getCellEditor(String fieldName, boolean retryElementNotFound, boolean retryStaleElement) {
 
         if (fieldMap == null) {
-
+            fieldMap = TestBaseApp.getDriver().findElements(By.tagName("table")).get(1).findElements(By.tagName("tr"))
+                    .stream()
+                    .collect(Collectors.toMap(webElement -> webElement.findElement(By.tagName("td")).getText(), webElement -> webElement.findElements(By.tagName("td")).get(1)));
         }
-        return null;
+        WebElement webElement = fieldMap.get(fieldName);
+        if (webElement == null) {
+            if (retryElementNotFound) {
+                fieldMap = null;
+                return getCellEditor(fieldName, false, retryStaleElement);
+            }
+            Assert.fail("Field " + fieldName + " not found");
+            throw new RuntimeException("Field " + fieldName + " not found");
+        }
+        try {
+            webElement.getTagName();
+        } catch (StaleElementReferenceException e) {
+            if (retryStaleElement) {
+                fieldMap = null;
+                return getCellEditor(fieldName, retryElementNotFound, false);
+            }
+            throw e;
+        }
+        return webElement;
     }
 }
